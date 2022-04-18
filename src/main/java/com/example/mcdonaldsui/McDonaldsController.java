c797c881291a83dd204539d06ecddd88e0e5e0da
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-04-18 20:46:36 +0300

    gui lookin fine jk its shit

--- src/main/java/com/example/mcdonaldsui/McDonaldsController.java
+++ src/main/java/com/example/mcdonaldsui/McDonaldsController.java
@@ -1,14 +1,10 @@
 package com.example.mcdonaldsui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class McDonaldsController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
 }
