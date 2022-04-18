3c94241b16455fc7dbf3eeb2f8136a1a70c8af2c
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-04-18 16:20:31 +0300

    sence buillder is working ezzzz

--- src/main/java/com/example/mcdonaldsui/McDonaldsApplication.java
+++ src/main/java/com/example/mcdonaldsui/McDonaldsApplication.java
@@ -2,40 +2,34 @@ package com.example.mcdonaldsui;
 
 import com.myProject.SelfServicesMechin.SelfServicesMechanic;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class McDonaldsApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.GRAY);
-        Image icon = new Image("file:src/mcdonad.png");
-
-        stage.getIcons().add(icon);
-        stage.setResizable(false);
-        stage.setWidth(420);
-        stage.setHeight(420);
-        stage.setFullScreen(false);
-        stage.setFullScreenExitHint("Welcome to mcdonalds");
+        Parent root = FXMLLoader.load(getClass().getResource("ben.fxml"));
         stage.setTitle("McDonalds");
-        stage.setScene(scene);
+        stage.setScene(new Scene(root,420,420));
         stage.show();
     }
 
 
     public static void main(String[] args) {
        launch();
-        SelfServicesMechanic selfServicesMechanic = new SelfServicesMechanic();
-        selfServicesMechanic.load();
-
-
-        selfServicesMechanic.orderDish("mcNuggets","regular");
+//        SelfServicesMechanic selfServicesMechanic = new SelfServicesMechanic();
+//        selfServicesMechanic.load();
+//
+//
+//        selfServicesMechanic.orderDish("mcNuggets","regular");
         //selfServicesMechanic.orderMeal("mcNuggets", "spicy","snacks","potato fries","beverage", "lean");
         // selfServicesMechanic.orderDish("mcNuggets", "spicy");
         //selfServicesMechanic.orderSideDish("beverage","lean");
